f3fa318dec7517bf61905ca5ef99b25303738ed7
David Vokoun <cachedfailure@gmail.com>
2014-02-18 11:42:32 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# js1k2014
+
+# build.js
+
+This is a quick nodejs builder script that takes a given js file, 
+minifies/compresses/mangles it, and injects it into the js1k shim.
+
+It depends on the node module uglify-js.
---
+++ build.js
@@ -0,0 +1,46 @@
+// build.js
+// david vokoun  <david@vokoun.me>
+
+// node build.js <scriptfile> <outputfile>
+//   scriptfile: the name of the script file in the js dir (without the .js)
+//   outputfile: the name of the file to write in the build dir
+
+// ex: node build.js test.js
+
+var fs = require('fs')
+  , UglifyJS = require('uglify-js')
+
+  , shim = "\r\n<!doctype html>\r\n<html>\r\n\t<head>\r\n\t\t<title>JS1k, [COMPO] demo submission [ID]</title>\r\n\t\t<meta charset=\"utf-8\" />\r\n    <style>\r\n      html, body { margin: 0; padding: 0; border: 0; }\r\n      #c { display: block; } /* kill scrollbars from hell */\r\n    </style>\r\n\t</head>\r\n\t<body>\r\n\t\t<canvas id=\"c\"></canvas>\r\n\t\t<script>\r\n\t\t\tvar a = document.getElementsByTagName('canvas')[0];\r\n\t\t\tvar b = document.body;\r\n      var d = function(e){ return function(){ e.parentNode.removeChild(e); }; }(a);\r\n      // unprefix some popular vendor prefixed things (but stick to their original name)\r\n      var AudioContext =\r\n        window.AudioContext ||\r\n        window.webkitAudioContext;\r\n      var requestAnimationFrame =\r\n        window.requestAnimationFrame ||\r\n        window.mozRequestAnimationFrame ||\r\n        window.webkitRequestAnimationFrame ||\r\n        window.msRequestAnimationFrame ||\r\n        function(f){ setTimeout(f, 1000/30); };\r\n      // stretch canvas to screen size (once, wont onresize!)\r\n      a.style.width = (a.width = innerWidth) + 'px';\r\n      a.style.height = (a.height = innerHeight) + 'px';\r\n\r\n      var c = a.getContext('2d');\r\n\t\t</script>\r\n\t\t<script>\r\n// start of submission //\r\n<%= script %>\r\n// end of submission //\r\n\t\t</script>\r\n\t</body>\r\n</html>\r\n"
+ 
+  , name = typeof process.argv[2] != undefined ? process.argv[2].replace('.js', '') : false
+  , script = name ? fs.readFileSync(name+'.js') : false
+  ;
+
+
+if(!script) {
+
+  console.log('ERROR: Missing argument: node build.js <scriptname>');
+
+} else {
+
+  var min = UglifyJS.minify(script.toString(), {mangle:true, fromString: true }).code
+
+    , output = shim.replace('<%= script %>', min)
+    , res = ''
+      ;
+
+  fs.writeFileSync( name + '.html', output);  
+
+  res = 'OK: '+ 
+        'Script Size: ['+min.length+' bytes], '+
+        'Output: ['+name+'.html]\n';
+
+  if(min.length > 1024)
+    res = 'WARN: '+
+          'Script size: ['+min.length+' bytes] '+
+          '(exceeds 1kb limit by '+(min.length-1024)+' bytes), '+
+          'Output: ['+name+'.html] \n';
+
+  console.log(res);
+  
+}
