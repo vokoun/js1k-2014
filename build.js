3c8afe07fb38286ce31c8ddbd0c4d20c536fcf57
David Vokoun <cachedfailure@gmail.com>
2014-02-18 23:17:21 -0500

    strip windows style from linebreaks on non windows platforms

--- build.js
+++ build.js
@@ -9,11 +9,15 @@
 var fs = require('fs')
   , UglifyJS = require('uglify-js')
 
-  , shim = "\r\n<!doctype html>\r\n<html>\r\n\t<head>\r\n\t\t<title>JS1k, [COMPO] demo submission [ID]</title>\r\n\t\t<meta charset=\"utf-8\" />\r\n    <style>\r\n      html, body { margin: 0; padding: 0; border: 0; }\r\n      #c { display: block; } /* kill scrollbars from hell */\r\n    </style>\r\n\t</head>\r\n\t<body>\r\n\t\t<canvas id=\"c\"></canvas>\r\n\t\t<script>\r\n\t\t\tvar a = document.getElementsByTagName('canvas')[0];\r\n\t\t\tvar b = document.body;\r\n      var d = function(e){ return function(){ e.parentNode.removeChild(e); }; }(a);\r\n      // unprefix some popular vendor prefixed things (but stick to their original name)\r\n      var AudioContext =\r\n        window.AudioContext ||\r\n        window.webkitAudioContext;\r\n      var requestAnimationFrame =\r\n        window.requestAnimationFrame ||\r\n        window.mozRequestAnimationFrame ||\r\n        window.webkitRequestAnimationFrame ||\r\n        window.msRequestAnimationFrame ||\r\n        function(f){ setTimeout(f, 1000/30); };\r\n      // stretch canvas to screen size (once, wont onresize!)\r\n      a.style.width = (a.width = innerWidth) + 'px';\r\n      a.style.height = (a.height = innerHeight) + 'px';\r\n\r\n      var c = a.getContext('2d');\r\n\t\t</script>\r\n\t\t<script>\r\n// start of submission //\r\n<%= script %>\r\n// end of submission //\r\n\t\t</script>\r\n\t</body>\r\n</html>\r\n"
+  , shim = "<!doctype html>\r\n<html>\r\n\t<head>\r\n\t\t<title>JS1k, [COMPO] demo submission [ID]</title>\r\n\t\t<meta charset=\"utf-8\" />\r\n    <style>\r\n      html, body { margin: 0; padding: 0; border: 0; }\r\n      #c { display: block; } /* kill scrollbars from hell */\r\n    </style>\r\n\t</head>\r\n\t<body>\r\n\t\t<canvas id=\"c\"></canvas>\r\n\t\t<script>\r\n\t\t\tvar a = document.getElementsByTagName('canvas')[0];\r\n\t\t\tvar b = document.body;\r\n      var d = function(e){ return function(){ e.parentNode.removeChild(e); }; }(a);\r\n      // unprefix some popular vendor prefixed things (but stick to their original name)\r\n      var AudioContext =\r\n        window.AudioContext ||\r\n        window.webkitAudioContext;\r\n      var requestAnimationFrame =\r\n        window.requestAnimationFrame ||\r\n        window.mozRequestAnimationFrame ||\r\n        window.webkitRequestAnimationFrame ||\r\n        window.msRequestAnimationFrame ||\r\n        function(f){ setTimeout(f, 1000/30); };\r\n      // stretch canvas to screen size (once, wont onresize!)\r\n      a.style.width = (a.width = innerWidth) + 'px';\r\n      a.style.height = (a.height = innerHeight) + 'px';\r\n\r\n      var c = a.getContext('2d');\r\n\t\t</script>\r\n\t\t<script>\r\n// start of submission //\r\n<%= script %>\r\n// end of submission //\r\n\t\t</script>\r\n\t</body>\r\n</html>\r\n"
  
   , name = typeof process.argv[2] != undefined ? process.argv[2].replace('.js', '') : false
   , script = name ? fs.readFileSync(name+'.js') : false
   ;
+
+// strip \r from \r\n on non windows platforms
+if(process.platform !== 'win32')
+  shim = shim.replace('\r','');
 
 
 if(!script) {
